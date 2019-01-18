78415b27e4e1452fcb70eb1bebf306e15fec5681
Nic Findlay <nic@visualclickstudio.com>
2019-01-18 10:47:25 +0200

    Added a little error handeling

--- index.php
+++ index.php
@@ -4,30 +4,45 @@
 require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 
-$file_name = $_FILES['file']['name'];   
-$temp_file_location = $_FILES['file']['tmp_name']; 
 
+if(isset($_FILES['file'])){
+    $file_name = $_FILES['file']['name'];   
+    $temp_file_location = $_FILES['file']['tmp_name']; 
 
-// Configure a client using Spaces
-$client = new Aws\S3\S3Client([
-        'version' => 'latest',
-        'region'  => 'ams3',
-        'endpoint' => 'https://ams3.digitaloceanspaces.com',
-        'credentials' => [
-                'key'    => 'YOUR ACCESS KEY',  //Change this
-                'secret' => 'YOUR SECRET KEY',  //Change this
-            ],
-        'http'    => [
-            'verify' => 'LOCATION OF YOUR CA CERTIFICATE'
-        ]
-]);
+    //If file_name is empty then give it a no name
+    if( strlen($file_name) == 0){
+      $file_name = "no-name-brand.epub";
+    }
 
-// Upload a file to the Space
-$insert = $client->putObject([
-     'Bucket' => 'YOUR BUCKET NAME',    //Change this
-     'Key'    => $file_name,
-     'Body'   => 'The contents of the file'
-]);
+    // Configure a client using Spaces
+    $client = new Aws\S3\S3Client([
+            'version' => 'latest',
+            'region'  => 'ams3',
+            'endpoint' => 'https://ams3.digitaloceanspaces.com',
+            'credentials' => [
+                    'key'    => 'YOUR ACCESS KEY',  //Change this
+                    'secret' => 'YOUR SECRET KEY',  //Change this
+                ],
+            'http'    => [
+                'verify' => 'LOCATION OF YOUR CA CERTIFICATE'
+            ]
+    ]);
+
+    // Upload a file to the Space
+    $insert = $client->putObject([
+         'Bucket' => 'YOUR BUCKET NAME',    //Change this
+         'Key'    => $file_name,
+         'Body'   => 'The contents of the file'
+    ]);
+
+    //Post Success redirection
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+      header("Location: /dollarbooks/upload/success.html");
+    }
+    else{
+      header("Location: /dollarbooks/upload/failure.html");
+    }
+}
 ?>
 
 <html lang="en">
